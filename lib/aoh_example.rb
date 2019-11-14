ef0bd28521e3e6a1073d2012a39cf4c09b9b286a
John Rogala <johnrossrogala@gmail.com>
2019-11-14 04:43:42 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,7 +18,7 @@ def literal_aoh
   
   literal_array = [
     {name: "Don Gately", occupation: "Live-in Staff"},
-    {name: "Joelle van Dayne", occupation: "Radio Personality"}, 
+    {name: "Joelle van Dyne", occupation: "Radio Personality"}, 
     {name: "Pat Monteseian", occupation: "Staff"},
     {name: "Kate Gompert", occupation: "None"},
     {name: "Bruce Green", occupation: "Fan of Mildred"}]
